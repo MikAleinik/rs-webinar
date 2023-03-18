9d7dd5d28f7d5a49811501643c53543b29b5e143
Mikhail Aleinik <6424156@tut.by>
2023-03-19 01:00:01 +0300

    refactor: implements callback and listener code example in presetation page

--- presetation/index.js
+++ presetation/index.js
@@ -10,8 +10,6 @@ window.addEventListener('load', () => {
   const elementsClear = document.querySelectorAll('.button__clear');
   elementsClear.forEach((element) => element.addEventListener('click', clearAllConsoles));
 
-  document.getElementById('syncBtn').addEventListener('click', runSync);
-  document.getElementById('asyncBtn').addEventListener('click', runAsync);
 });
 
 function addLog(id, message) {
@@ -27,7 +25,69 @@ function clearAllConsoles() {
   });
 }
 
+let buttonRemoveFuncTwo = document.getElementById('buttonRemoveFuncTwo');
+buttonRemoveFuncTwo.addEventListener('mouseenter', () => {
+  addLog('removeFuncTwoLog', 'Первый обработчик.(1)');
+});
+buttonRemoveFuncTwo.addEventListener('mouseout', mouseOutTwoHandler);
+function mouseOutTwoHandler() {
+  addLog('removeFuncTwoLog', 'Второй обработчик.(2)');
+}
+buttonRemoveFuncTwo.addEventListener('click', () => {
+  buttonRemoveFuncTwo.removeEventListener('mouseenter', () => {
+    addLog('removeFuncTwoLog', 'Первый обработчик.(1)');
+  });
+  buttonRemoveFuncTwo.removeEventListener('mouseout', mouseOutTwoHandler);
+});
 
+let buttonRemoveFunc = document.getElementById('buttonRemoveFunc');
+buttonRemoveFunc.addEventListener('mouseenter', mouseEnterHandler);
+buttonRemoveFunc.addEventListener('mouseout', mouseOutHandler);
+function mouseEnterHandler() {
+  addLog('removeFuncLog', 'Первый обработчик.(1)');
+}
+function mouseOutHandler() {
+  addLog('removeFuncLog', 'Второй обработчик.(2)');
+}
+buttonRemoveFunc.addEventListener('click', () => {
+  buttonRemoveFunc.removeEventListener('mouseenter', mouseEnterHandler);
+});
+
+let buttonDifferentEvent = document.getElementById('buttonDifferentEvent');
+buttonDifferentEvent.addEventListener('click', () => {
+  addLog('differentEventLog', 'Первый обработчик.(1)');
+});
+buttonDifferentEvent.addEventListener('mouseenter', () => {
+  addLog('differentEventLog', 'Второй обработчик.(2)');
+});
+buttonDifferentEvent.addEventListener('mouseout', () => {
+  addLog('differentEventLog', 'Третий обработчик.(3)');
+});
+
+let buttonManyListener = document.getElementById('buttonManyListener');
+buttonManyListener.addEventListener('click', () => {
+  addLog('addManyListenerLog', 'Первый обработчик.(1)');
+});
+buttonManyListener.addEventListener('click', () => {
+  addLog('addManyListenerLog', 'Второй обработчик.(2)');
+});
+buttonManyListener.addEventListener('click', () => {
+  addLog('addManyListenerLog', 'Третий обработчик.(3)');
+});
+
+document.getElementById('timerBtn').addEventListener('click', runAsyncTimer);
+function runAsyncTimer() {
+  function timer() {
+    setTimeout(() => {
+      addLog('timerLog', 'Завершение длительных вычислений.(2)');
+    }, 1000);
+  }
+  addLog('timerLog', 'Начало выполнения кода.(1)');
+  timer();
+  addLog('timerLog', 'Завершение выполнения кода.(3)');
+}
+
+document.getElementById('asyncBtn').addEventListener('click', runAsync);
 function runAsync() {
   async function summ() {
     let result = await function () {
@@ -44,6 +104,8 @@ function runAsync() {
   summ();
   addLog('asyncLog', 'Завершение выполнения кода.(3)');
 }
+
+document.getElementById('syncBtn').addEventListener('click', runSync);
 function runSync() {
   function summ() {
     let counter = 0;
@@ -56,4 +118,29 @@ function runSync() {
   addLog('syncLog', 'Начало выполнения кода.(1)');
   summ();
   addLog('syncLog', 'Завершение выполнения кода.(3)');
+}
+
+document.getElementById('callbackBtn').addEventListener('click', runCallback);
+function runCallback() {
+  function summ(numberOne, numberTwo) {
+    addLog('callbackLog', `${numberOne} + ${numberTwo}`);
+    return numberOne + numberTwo;
+  }
+  function difference(numberOne, numberTwo) {
+    addLog('callbackLog', `${numberOne} - ${numberTwo}`);
+    return numberOne - numberTwo;
+  }
+  function action(numbers, callbacks) {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i += 1) {
+      const currentCallback = callbacks[i - 1];
+      const number = numbers[i];
+      result = currentCallback(result, number);
+      addLog('callbackLog', `${result}`);
+    }
+    return result;
+  }
+  const numbers = [1, 2, 3, 4];
+  const callbacks = [summ, difference, summ];
+  addLog('callbackLog', `Результат = ${action(numbers, callbacks)}`);
 }
