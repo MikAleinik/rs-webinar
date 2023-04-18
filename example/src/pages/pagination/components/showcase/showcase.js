85ab275530c6648f8e7b3d863111221fff8600e9
MikAleinik <6424156@tut.by>
2023-04-18 10:21:31 +0300

    refactor: fixed markup

--- example/src/pages/pagination/components/showcase/showcase.js
+++ example/src/pages/pagination/components/showcase/showcase.js
@@ -62,8 +62,8 @@ function createRandomPets(pets) {
     return petsResult;
 }
 function getRandomNumber(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 };
 export { createComponent, showPage };
--- example/src/pages/slider/components/slider.js
+++ example/src/pages/slider/components/slider.js
@@ -2,11 +2,11 @@ import './slider.css';
 import * as Card from '../../shared/card/card';
 
 const CssClasses = {
-	SLIDER: 'slider',
-	BUTTON: 'round__button',
-	WRAPPER: 'slider__wrapper',
-	CARD_CONTAINER: 'card__container',
-	CARD_GROUP: 'card__group',
+    SLIDER: 'slider',
+    BUTTON: 'round__button',
+    WRAPPER: 'slider__wrapper',
+    CARD_CONTAINER: 'card__container',
+    CARD_GROUP: 'card__group',
     ANIMATE_LEFT: 'animate__left',
     ANIMATE_RIGHT: 'animate__right',
     NO_TRANSITION: 'container__no-transition',
@@ -32,144 +32,144 @@ function createComponent(petsJSON) {
         throw TypeError(`Slider error. Pets array is invalid.`);
     }
 
-	const component = createElement('section', CssClasses.SLIDER);
+    const component = createElement('section', CssClasses.SLIDER);
 
-	buttonLeft = createElement('button', CssClasses.BUTTON);
-	buttonLeft.textContent = TEXT_BUTTON_LEFT;
-	component.append(buttonLeft);
+    buttonLeft = createElement('button', CssClasses.BUTTON);
+    buttonLeft.textContent = TEXT_BUTTON_LEFT;
+    component.append(buttonLeft);
 
-	const wrapper = createElement('div', CssClasses.WRAPPER);
+    const wrapper = createElement('div', CssClasses.WRAPPER);
 
-	cardContainer = createElement('ul', CssClasses.CARD_CONTAINER);
-	wrapper.append(cardContainer);
+    cardContainer = createElement('ul', CssClasses.CARD_CONTAINER);
+    wrapper.append(cardContainer);
 
-	petsJSON.forEach((pet) => {
-		const cardComponent = Card.createComponent(pet);
-		cards.push(cardComponent);
-	});
+    petsJSON.forEach((pet) => {
+        const cardComponent = Card.createComponent(pet);
+        cards.push(cardComponent);
+    });
 
-	for (let i = 0; i < COUNT_CARD_BLOCK; i += 1) {
-		const cardGroup = createElement('ul', CssClasses.CARD_GROUP);
-		cardGroup.style.order = `${i + 1}`;
-		cardContainer.append(cardGroup);
-	}
-	for (let i = 0; i < COUNT_CARD; i += 1) {
-		cardContainer.children[1].append(cards[i]);
-		visibleCards.push(cards[i]);
-	}
+    for (let i = 0; i < COUNT_CARD_BLOCK; i += 1) {
+        const cardGroup = createElement('ul', CssClasses.CARD_GROUP);
+        cardGroup.style.order = `${i + 1}`;
+        cardContainer.append(cardGroup);
+    }
+    for (let i = 0; i < COUNT_CARD; i += 1) {
+        cardContainer.children[1].append(cards[i]);
+        visibleCards.push(cards[i]);
+    }
 
-	component.append(wrapper);
+    component.append(wrapper);
 
-	buttonRight = createElement('button', CssClasses.BUTTON);
-	buttonRight.textContent = TEXT_BUTTON_RIGHT;
-	component.append(buttonRight);
+    buttonRight = createElement('button', CssClasses.BUTTON);
+    buttonRight.textContent = TEXT_BUTTON_RIGHT;
+    component.append(buttonRight);
 
-	buttonLeft.addEventListener('click', buttonLeftClickHandler);
-	buttonRight.addEventListener('click', buttonRightClickHandler);
-	cardContainer.addEventListener('transitionend', endTransitionHandler);
+    buttonLeft.addEventListener('click', buttonLeftClickHandler);
+    buttonRight.addEventListener('click', buttonRightClickHandler);
+    cardContainer.addEventListener('transitionend', endTransitionHandler);
 
-	return component;
+    return component;
 };
 function createElement(tagName, className) {
-	const element = document.createElement(tagName);
-	element.classList.add(className);
-	return element;
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    return element;
 }
 function buttonLeftClickHandler() {
-	if (!isRightClick) {
-		let rightCardGroup = null;
-		for (let group of cardContainer.children) {
-			if (Number(group.style.order) === INDEX_VISIBLE_GROUP + 1) {
-				rightCardGroup = group;
-				break;
-			}
-		}
-		while (rightCardGroup.firstElementChild) {
-			rightCardGroup.firstElementChild.remove();
-		}
+    if (!isRightClick) {
+        let rightCardGroup = null;
+        for (let group of cardContainer.children) {
+            if (Number(group.style.order) === INDEX_VISIBLE_GROUP + 1) {
+                rightCardGroup = group;
+                break;
+            }
+        }
+        while (rightCardGroup.firstElementChild) {
+            rightCardGroup.firstElementChild.remove();
+        }
 
-		const cardsToShow = getCardsToShow();
-		visibleCards = new Array(...cardsToShow);
-		cardsToShow.forEach((card) => {
-			rightCardGroup.insertAdjacentElement('beforeend', card);
-		});
-	}
+        const cardsToShow = getCardsToShow();
+        visibleCards = new Array(...cardsToShow);
+        cardsToShow.forEach((card) => {
+            rightCardGroup.insertAdjacentElement('beforeend', card);
+        });
+    }
 
-	isLeftClick = true;
-	isRightClick = !isLeftClick;
+    isLeftClick = true;
+    isRightClick = !isLeftClick;
 
-	deltaOrder = -1;
-	cardContainer.classList.add(CssClasses.ANIMATE_LEFT);
-	disableButtons();
+    deltaOrder = -1;
+    cardContainer.classList.add(CssClasses.ANIMATE_LEFT);
+    disableButtons();
 };
 function buttonRightClickHandler() {
-	if (!isLeftClick) {
-		let leftCardGroup = null;
-		for (let group of cardContainer.children) {
-			if (Number(group.style.order) === INDEX_VISIBLE_GROUP - 1) {
-				leftCardGroup = group;
-				break;
-			}
-		}
-		while (leftCardGroup.firstElementChild) {
-			leftCardGroup.firstElementChild.remove();
-		}
+    if (!isLeftClick) {
+        let leftCardGroup = null;
+        for (let group of cardContainer.children) {
+            if (Number(group.style.order) === INDEX_VISIBLE_GROUP - 1) {
+                leftCardGroup = group;
+                break;
+            }
+        }
+        while (leftCardGroup.firstElementChild) {
+            leftCardGroup.firstElementChild.remove();
+        }
 
-		const cardsToShow = getCardsToShow();
-		visibleCards = new Array(...cardsToShow);
-		cardsToShow.forEach((card) => {
-			leftCardGroup.insertAdjacentElement('beforeend', card);
-		});
-	}
+        const cardsToShow = getCardsToShow();
+        visibleCards = new Array(...cardsToShow);
+        cardsToShow.forEach((card) => {
+            leftCardGroup.insertAdjacentElement('beforeend', card);
+        });
+    }
 
-	isRightClick = true;
-	isLeftClick = !isRightClick;
+    isRightClick = true;
+    isLeftClick = !isRightClick;
 
-	deltaOrder = 1;
-	cardContainer.classList.add(CssClasses.ANIMATE_RIGHT);
-	disableButtons();
+    deltaOrder = 1;
+    cardContainer.classList.add(CssClasses.ANIMATE_RIGHT);
+    disableButtons();
 };
 function disableButtons() {
-	buttonLeft.setAttribute('disabled', true);
-	buttonRight.setAttribute('disabled', true);
+    buttonLeft.setAttribute('disabled', true);
+    buttonRight.setAttribute('disabled', true);
 }
 function enableButtons() {
-	buttonLeft.removeAttribute('disabled');
-	buttonRight.removeAttribute('disabled');
+    buttonLeft.removeAttribute('disabled');
+    buttonRight.removeAttribute('disabled');
 }
 function endTransitionHandler() {
     cardContainer.classList.add(CssClasses.NO_TRANSITION);
-	for (const group of cardContainer.children) {
-		let order = Number(group.style.order);
-		order = order + deltaOrder;
-		if (order <= 0) {
-			order = COUNT_CARD;
-		} else if (order > COUNT_CARD) {
-			order = 1;
-		}
-		group.style.order = order;
-	}
+    for (const group of cardContainer.children) {
+        let order = Number(group.style.order);
+        order = order + deltaOrder;
+        if (order <= 0) {
+            order = COUNT_CARD;
+        } else if (order > COUNT_CARD) {
+            order = 1;
+        }
+        group.style.order = order;
+    }
 
     cardContainer.classList.remove(CssClasses.ANIMATE_LEFT, CssClasses.ANIMATE_RIGHT);
-	setTimeout(() => {
+    setTimeout(() => {
         cardContainer.classList.remove(CssClasses.NO_TRANSITION);
-		enableButtons();
-	}, 1);
+        enableButtons();
+    }, 1);
 };
 function getCardsToShow() {
-	let cardsToShow = new Array();
-	while (cardsToShow.length < COUNT_CARD) {
-		const index = getRandomNumber(0, cards.length - 1);
-		if (!visibleCards.includes(cards[index]) && !cardsToShow.includes(cards[index])) {
-			cardsToShow.push(cards[index]);
-		}
-	}
-	return cardsToShow;
+    let cardsToShow = new Array();
+    while (cardsToShow.length < COUNT_CARD) {
+        const index = getRandomNumber(0, cards.length - 1);
+        if (!visibleCards.includes(cards[index]) && !cardsToShow.includes(cards[index])) {
+            cardsToShow.push(cards[index]);
+        }
+    }
+    return cardsToShow;
 };
 function getRandomNumber(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 };
 
 export { createComponent };
