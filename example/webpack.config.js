33f898d0023b65290a50d7539161eaed13b8fee8
MikAleinik <6424156@tut.by>
2023-05-05 16:34:37 +0300

    feat: added default code example files

---
+++ example/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const baseConfig = {
+    entry: {
+        slider: './src/pages/slider/app-slider.js',
+        pagination: './src/pages/pagination/app-pagination.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            chunks: ['index'],
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'assets'),
+                    to: path.resolve(__dirname, 'dist', 'assets'),
+                },
+            ],
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        splitChunks: {
+            name: 'common',
+            chunks: 'all',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+};
+
+module.exports = ({}, { mode }) => {
+    const isProductionMode = mode === 'production';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
